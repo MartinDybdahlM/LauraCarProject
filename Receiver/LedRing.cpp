7e5b9730327309f9bb67f7f4e909b0addf8abf03
martindybdahlm <martin.dybdahl@lego.com>
2025-07-14 16:29:10 +0200

    Rainbow colors for the LEDs

--- Receiver/LedRing.cpp
+++ Receiver/LedRing.cpp
@@ -2,6 +2,21 @@
 
 #define NUM_LEDS 12
 
+uint32_t rainbowColors[NUM_LEDS] = {
+  0xFF0000,  // Red
+  0xFF4500,  // Orange Red
+  0xFF8C00,  // Dark Orange
+  0xFFD700,  // Gold
+  0xFFFF00,  // Yellow
+  0x9AFF9A,  // Light Green
+  0x00FF00,  // Green
+  0x00FF7F,  // Spring Green
+  0x00FFFF,  // Cyan
+  0x0080FF,  // Sky Blue
+  0x0000FF,  // Blue
+  0x8A2BE2   // Blue Violet
+};
+
 LedRing::LedRing(int pin) : ring(NUM_LEDS, pin, NEO_GRB + NEO_KHZ800) {
   speedValue = 0;
 }
@@ -20,15 +35,7 @@ void LedRing::update() {
   int ledsOn = map(speedValue, 0, 255, 0, NUM_LEDS);
 
   for (int i = 0; i < ledsOn; i++) {
-    uint32_t color;
-    if (i < 4)
-      color = ring.Color(255, 30, 30);
-    else if (i < 8)
-      color = ring.Color(180, 0, 0);
-    else
-      color = ring.Color(80, 0, 0);
-
-    ring.setPixelColor(i, color);
+    ring.setPixelColor(i, rainbowColors[i]);
   }
 
   ring.show();
